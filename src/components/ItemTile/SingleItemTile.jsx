5f299861fa36b59021efbe4a5ff684321d80899b
NaashNix <thilakshanaravindu@gmail.com>
2023-02-23 12:32:12 +0530

    SingleFoodItem updated

--- src/components/ItemTile/SingleItemTile.jsx
+++ src/components/ItemTile/SingleItemTile.jsx
@@ -3,12 +3,14 @@ import classes from './SingleItemTile.module.css';
 
 const SingleItemTile = () => {
    return (
-      <div className={classes.container} >
-         <span className={classes.timeTitle} >15 Minutes</span>
-         <span className={classes.mainTitle} >Roast Chicken</span>
-         <span className={classes.subTitle} >Rs. 500.00</span>
-      </div>
-   );
+		<div className={classes.container}>
+			<span className={classes.timeTitle}>15 Minutes</span>
+			<div className={classes.foodTitles} >
+				<span className={classes.mainTitle}>Roast Chicken Buriyani</span>
+				<span className={classes.subTitle}>Rs. 500.00</span>
+			</div>
+		</div>
+	);
 }
 
 export default SingleItemTile;
